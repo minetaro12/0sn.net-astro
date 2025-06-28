ac1ff574cfc332b759eaf153344ef8a896dd5f48
minetaro12 <77948036+minetaro12@users.noreply.github.com>
2025-06-28 18:40:23 +0900

    サイトマップを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import solidJs from '@astrojs/solid-js'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://0sn.net',
   trailingSlash: 'always',
 
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [solidJs()],
+  integrations: [solidJs(), sitemap()],
 })
