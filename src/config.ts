461bacdb9609f84d7195fd8e5c44c5dd625607c9
minetaro12 <77948036+minetaro12@users.noreply.github.com>
2025-07-03 03:33:12 +0000

    元のマークダウンファイルへのリンクを追加

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 const config = {
   title: 'minetaro12',
   description: 'Oさんのページ',
-  url: 'https://0sn.net'
+  url: 'https://0sn.net',
+  repoContent: ({ dateSlug, postSlug }: { dateSlug: string, postSlug: string }) => {
+    return `https://github.com/minetaro12/0sn.net-astro/blob/master/content/posts/${dateSlug}/${postSlug}/index.md`
+  }
 }
 
 export default config
